ce265104bc93e3377794a3cd27062354e62de9fa
ymd <1946334058@qq.com>
2019-11-19 21:07:23 +0800

    ShellSort exchange

--- src/com/atguigu/sort/BubbleSort.java
+++ src/com/atguigu/sort/BubbleSort.java
@@ -1,38 +1,59 @@
 package com.atguigu.sort;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 public class BubbleSort {
     public static void main(String[] args) {
-//        int arr[] = {3,9,-1,10,-2};
-//        bubboSort(arr);
-//        System.out.println("排序之后");
-//        System.out.println(Arrays.toString(arr));
-
-        int[] arr = new int[80000];
-
-        for(int i=0; i<80000; i++){
-            arr[i] = (int) (Math.random() * 80000);
-        }
-        Date date = new Date();
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String format1 = format.format(date);
-        System.out.println("排序前："+format1);
+        int arr[] = {3,9,-1,10,-2};
         bubboSort(arr);
-        Date date1 = new Date();
-        SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String format3 = format2.format(date1);
-        System.out.println("排序前："+format3);
+        System.out.println("排序之后");
+        System.out.println(Arrays.toString(arr));
+
+//        int[] arr = new int[80000];
+//
+//        for(int i=0; i<80000; i++){
+//            arr[i] = (int) (Math.random() * 80000);
+//        }
+//        Date date = new Date();
+//        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        String format1 = format.format(date);
+//        System.out.println("排序前："+format1);
+//        bubboSort(arr);
+//        Date date1 = new Date();
+//        SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        String format3 = format2.format(date1);
+//        System.out.println("排序前："+format3);
 
     }
 
     public static void bubboSort(int []arr){
+//        int temp;
+//        boolean flag = false;
+//        for(int i=0; i<arr.length-1; i++){
+//            for(int j=0; j<arr.length-1-i; j++){
+//                if(arr[j]>arr[j+1]){
+//                    flag = true;
+//                    temp = arr[j];
+//                    arr[j] = arr[j+1];
+//                    arr[j+1] = temp;
+//                }
+//            }
+//            if(!flag){
+//                break;
+//            }else {
+//                flag = false;
+//            }
+//
+//        }
+
+
         int temp;
-        boolean flag = false;
-        for(int i=0; i<arr.length-1; i++){
-            for(int j=0; j<arr.length-1-i; j++){
-                if(arr[j]>arr[j+1]){
+        for(int i=0; i<arr.length; i++){
+            boolean flag = false;
+            for(int j=0; j<arr.length-i-1; j++){
+                if(arr[j] > arr[j+1]){
                     flag = true;
                     temp = arr[j];
                     arr[j] = arr[j+1];
@@ -41,10 +62,9 @@ public class BubbleSort {
             }
             if(!flag){
                 break;
-            }else {
-                flag = false;
             }
-
         }
+
+
     }
 }
--- src/com/atguigu/sort/SelectSort.java
+++ src/com/atguigu/sort/SelectSort.java
@@ -1,31 +1,32 @@
 package com.atguigu.sort;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 public class SelectSort {
     public static void main(String[] args) {
-//        int[] arr = {101, 34, 119, 1, -1, 90, 123};
-//        System.out.println("排序前");
-//        System.out.println(Arrays.toString(arr));
+        int[] arr = {101, 34, 119, 1, -1, 90, 123};
+        System.out.println("排序前");
+        System.out.println(Arrays.toString(arr));
+        selectSort1(arr);
+        System.out.println("排序后");
+        System.out.println(Arrays.toString(arr));
+
+//        int[] arr = new int[80000];
+//
+//        for (int i = 0; i < 80000; i++) {
+//            arr[i] = (int) (Math.random() * 80000);
+//        }
+//        Date date = new Date();
+//        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        String format1 = format.format(date);
+//        System.out.println("排序前：" + format1);
 //        selectSort(arr);
-//        System.out.println("排序后");
-//        System.out.println(Arrays.toString(arr));
-
-        int[] arr = new int[80000];
-
-        for(int i=0; i<80000; i++){
-            arr[i] = (int) (Math.random() * 80000);
-        }
-        Date date = new Date();
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String format1 = format.format(date);
-        System.out.println("排序前："+format1);
-        selectSort(arr);
-        Date date1 = new Date();
-        SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String format3 = format2.format(date1);
-        System.out.println("排序前："+format3);
+//        Date date1 = new Date();
+//        SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        String format3 = format2.format(date1);
+//        System.out.println("排序前：" + format3);
     }
 
     public static void selectSort(int[] arr) {
@@ -43,6 +44,21 @@ public class SelectSort {
                 arr[minIndex] = arr[i];
                 arr[i] = min;
             }
+        }
+    }
+
+    public static void selectSort1(int[] arr) {
+        for(int i=0; i<arr.length; i++){
+            int min = arr[i];
+            int minIndex = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(min > arr[j]){
+                    min = arr[j];
+                    minIndex = j;
+                }
+            }
+            arr[minIndex] = arr[i];
+            arr[i] = min;
         }
     }
 }
